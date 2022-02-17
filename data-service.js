f534dde1d2fa5648bae60970729db60d7a21f98f
SolovevArtem <soloviev.artyomemail@yandex.ru>
2022-02-17 20:02:52 +0300

    second

---
+++ data-service.js
@@ -0,0 +1,342 @@
+
+const Sequelize = require('sequelize');
+var sequelize = new Sequelize('dev4n8kjapc8va', 'pgvoedjnfycmrk', '66ff02484c1836aaf40ed4c057d945648c9638c667f71484fd1e70706c71c0e2', {
+    host: 'ec2-34-249-49-9.eu-west-1.compute.amazonaws.com', 
+    dialect: 'postgres', 
+    port: 5432, 
+    dialectOptions: {
+        ssl: true 
+    }
+});
+ 
+var People = sequelize.define('People', {
+    first_name: Sequelize.STRING,
+    last_name: Sequelize.STRING,
+    phone: Sequelize.STRING,
+    address: Sequelize.STRING,
+    city: Sequelize.STRING
+});
+
+var Car = sequelize.define('Car', {
+    vin: {type: Sequelize.STRING, primaryKey:true, unique:true},
+    make: Sequelize.STRING,
+    model: Sequelize.STRING,
+    year: Sequelize.STRING,
+});
+
+var Store = sequelize.define('Store', {
+    retailer: Sequelize.STRING,
+    phone: Sequelize.STRING,
+    address: Sequelize.STRING,
+    city: Sequelize.STRING,
+});
+
+Car.hasMany(People, {foreignKey:'vin'});
+
+module.exports.initalize = function(){
+    return new Promise(function (resolve, reject){ 
+        sequelize.sync()
+        .then((People) => {
+            resolve();
+        })
+        .then((Car) => {
+            resolve();
+        })
+        .then((Store) => {
+            resolve();
+        })
+        .catch((err) => {
+            reject("unable to sync the database");
+            return;
+        });
+    });
+};
+
+module.exports.getAllPeople = function(){
+    return new Promise(function (resolve, reject){ 
+        People.findAll()
+        .then((data)=>{
+            resolve(data);
+        })
+        .catch((err)=>{
+            reject("no results returned");
+        });
+    });
+};
+
+module.exports.getCars = function(){
+    return new Promise(function (resolve, reject){ 
+        Car.findAll()
+        .then((data)=>resolve(data))
+        .catch((err)=>{
+            reject("no results returned");
+        });
+    });
+};
+
+module.exports.getStores = function(){
+    return new Promise(function (resolve, reject){ 
+        Store.findAll()
+        .then((data)=>resolve(data))
+        .catch((err)=>{
+            reject("no results returned");
+        });
+    });
+};
+
+module.exports.addPeople = function(peopleData){
+    for (x in peopleData){
+        if (x=="") x=null;
+    }
+
+    return new Promise(function (resolve, reject){ 
+       People.create(peopleData)
+       .then(()=>{
+           resolve();
+       })
+       .catch(()=>{
+           reject("unable to create a person");
+       });
+    });
+};
+
+module.exports.addCar = function(carData){
+    for (x in carData){
+        if (x=="") x=null;
+    }
+
+    return new Promise(function (resolve, reject){ 
+      Car.create(carData)
+      .then(()=>{
+          resolve();
+        } )
+      .catch(()=> {
+          reject("unable to create car");
+        });
+    });
+};
+
+module.exports.addStore = function(storeData){
+    for (x in storeData){
+        if (x=="") x=null;
+    }
+
+    return new Promise(function (resolve, reject){ 
+      Store.create(storeData)
+      .then(()=>{
+          resolve();
+        } )
+      .catch(()=> {
+          reject("unable to create store");
+        });
+    });
+};
+
+module.exports.updateStore = function(storeData){
+    for (x in storeData){
+        if (x=="") x=null;
+    }
+
+    return new Promise(function (resolve, reject){ 
+      Store.update(storeData, {
+          where: {id: storeData.id}
+      })
+      .then(()=>{
+          resolve();
+        } )
+      .catch(()=> {
+          reject("unable to update store");
+        });
+    });
+};
+
+
+
+
+module.exports.updateCar = function(carData){
+    for (x in carData){
+        if (x=="") x=null;
+    }
+
+    return new Promise(function (resolve, reject){ 
+      Car.update(carData, {
+          where:{vin: carData.vin}
+      })
+      .then(()=>
+          resolve()
+      )
+      .catch(()=>{
+          reject("unable to create car")
+      })
+    });
+};
+
+
+module.exports.getPeopleByVin = function(Vin){
+    return new Promise(function (resolve, reject){ 
+        People.findAll({
+            where: {vin: Vin}
+        })
+        .then((data)=>{
+            resolve(data[0]);
+        })
+        .catch((err)=>{
+            reject("no results returned");
+        });
+    });
+};
+
+module.exports.getCarsByVin = function(Vin){
+    return new Promise(function (resolve, reject){ 
+        Car.findAll({
+            where: {vin: Vin}
+        })
+        .then((data)=>{
+            resolve(data);
+        })
+        .catch((err)=>{
+            reject("no results returned");
+        });
+    });
+};
+
+
+
+
+
+module.exports.getCarsByMake = function(make){
+    return new Promise(function (resolve, reject){ 
+        Car.findAll({
+            where: {make: make}
+        })
+        .then((data)=>{
+            resolve(data);
+        })
+        .catch((err)=>{
+            reject("no results returned");
+        });
+    });
+};
+
+module.exports.getCarsByYear = function(year){
+    return new Promise(function (resolve, reject){ 
+        Car.findAll({
+            where: {year: year}
+        })
+        .then((data)=>{
+            resolve(data);
+        })
+        .catch((err)=>{
+            reject("no results returned");
+        });
+    });
+};
+
+module.exports.getPeopleById = function(id){
+    return new Promise(function (resolve, reject){ 
+        People.findAll({
+            where: {id: id}
+        })
+        .then((data)=>{
+            resolve(data);
+        })
+        .catch((err)=>{
+            reject("no results returned");
+        });
+    });
+};
+
+module.exports.getStoreById = function(ident){
+ 
+    return new Promise(function (resolve, reject){ 
+      Store.findAll({
+          where: {id: ident}
+      })
+      .then((data)=>{
+          resolve(data);
+        } )
+      .catch(()=> {
+          reject("no results returned");
+        });
+    });
+};
+
+module.exports.deleteStoreById = function(ident){
+ 
+    return new Promise(function (resolve, reject){ 
+      Store.destroy({
+          where: {id: ident}
+      })
+      .then((data)=>{
+          resolve(data);
+        } )
+      .catch(()=> {
+          reject("unable to delete store");
+        });
+    });
+};
+
+module.exports.deleteCarByVin = function(Vin){
+    return new Promise(function (resolve, reject){ 
+        Car.destroy({
+            where: {vin: Vin}
+        })
+        .then((data)=>{
+            resolve(data)
+        })
+        .catch((err)=>{
+            reject("can not delete Car");
+        });
+
+    });
+};
+
+module.exports.deletePeopleById = function(ident){
+ 
+    return new Promise(function (resolve, reject){ 
+      People.destroy({
+          where: {id: ident}
+      })
+      .then((data)=>{
+          resolve(data);
+        } )
+      .catch(()=> {
+          reject("unable to delete store");
+        });
+    });
+};
+
+module.exports.getStoresByRetailer = function(retailer){
+    return new Promise(function (resolve, reject){ 
+        Store.findAll({
+            where: {retailer: retailer}
+        })
+        .then((data)=>{
+            resolve(data);
+        })
+        .catch((err)=>{
+            reject("no results returned");
+        });
+    });
+};
+
+module.exports.updatePerson = function(personData){
+    
+    for (x in personData){
+        if (x=="") x=null;
+    }
+
+    return new Promise(function (resolve, reject){ 
+       People.update(personData, {
+           where: {id: personData.id}
+       })
+       .then(()=>{
+           resolve();
+       })
+       .catch(()=>{
+            reject("unable to update person")
+       });
+    });
+};
+
+
---
+++ server.js
@@ -0,0 +1,296 @@
+
+
+const express = require("express");
+const path = require("path");
+const data = require("./data-service.js");
+const bodyParser = require('body-parser');
+const fs = require("fs");
+const multer = require("multer");
+const exphbs = require('express-handlebars');
+const app = express();
+
+const HTTP_PORT = process.env.PORT || 8080;
+
+function onHttpStart() {
+  console.log("Express http server listening on " + HTTP_PORT);
+}
+
+app.engine('.hbs', exphbs({ 
+    extname: '.hbs',
+    defaultLayout: "main",
+    helpers: { 
+        navLink: function(url, options){
+            return '<li' + 
+                ((url == app.locals.activeRoute) ? ' class="active" ' : '') + 
+                '><a href="' + url + '">' + options.fn(this) + '</a></li>';
+        }
+    } 
+}));
+
+app.set('view engine', '.hbs');
+
+
+// multer requires a few options to be setup to store files with file extensions
+// by default it won't store extensions for security reasons
+const storage = multer.diskStorage({
+    destination: "./public/pictures/uploaded",
+    filename: function (req, file, cb) {
+      // we write the filename as the current date down to the millisecond
+      // in a large web service this would possibly cause a problem if two people
+      // uploaded an image at the exact same time. A better way would be to use GUID's for filenames.
+      // this is a simple example.
+      cb(null, Date.now() + path.extname(file.originalname));
+    }
+  });
+  
+// tell multer to use the diskStorage function for naming files instead of the default.
+const upload = multer({ storage: storage });
+
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(function(req,res,next){
+    let route = req.baseUrl + req.path;
+    app.locals.activeRoute = (route == "/") ? "/" : route.replace(/\/$/, "");
+    next();
+});
+
+app.get("/", (req,res) => {
+    res.render("home");
+});
+
+app.get("/about", (req,res) => {
+    res.render("about");
+});
+
+app.get("/pictures/add", (req,res) => {
+    res.render("addImage");
+});
+
+app.get("/pictures", (req,res) => {
+    fs.readdir("./public/pictures/uploaded", function(err, items) {
+        res.render("pictures", {photo :items});
+    });
+});
+app.post("/pictures/add", upload.single("pictureFile"), (req,res) =>{
+    res.redirect("/pictures");
+});
+
+
+/*People routes */
+app.get("/people", (req,res) => {
+    if (req.query.vin){
+        data.getPeopleByVin(req.query.vin).then((data)=>{
+            res.render("people", (data.length > 0) ? {people:data.map(value => value.dataValues)} : { message: "no results" });
+        }).catch((err)=>{res.render("people",{ message: "no results" });
+    });
+    }
+    else{
+        data.getAllPeople().then((data)=>{
+            res.render("people", (data.length > 0) ? {people: data.map(value => value.dataValues)} : { message: "no results" });   
+        }).catch((err)=>{res.render("people",{ message: "no results" });
+    });
+    }
+});
+app.get("/people/add", (req,res) => {
+    data.getCars().then((data)=>{
+        res.render("addPeople", {cars: data.map(value => value.dataValues)});
+    }).catch((err) => {
+        // set cars list to empty array
+        res.render("addPeople", {cars: [] });
+     });
+});
+
+app.post("/people/add", (req, res) => {
+    data.addPeople(req.body).then(()=>{
+      res.redirect("/people");
+    }).catch((err)=>{
+        res.status(500).send("Unable to Add the Person");
+      });;
+});
+
+app.get("/car/:vin", (req,res)=>{
+    data.getCarsByVin(req.params.vin).then((data)=>{
+        res.render("car", {car:data.map(value => value.dataValues)[0]});
+    }).catch(()=>{
+        res.status(404).send("Car Not Found");
+    });
+});
+
+app.get("/person/:id", (req, res) => {
+
+    // initialize an empty object to store the values
+    let viewData = {};
+
+    data.getPeopleById(req.params.id).then((data) => {
+        if (data) {
+            viewData.person = data.map(value => value.dataValues)[0];
+             //store person data in the "viewData" object as "person"
+        } else {
+            viewData.person = null; // set person to null if none were returned
+        }
+    }).then(data.getCars)
+    .then((data) => {
+            // store cars data in the "viewData" object as "cars"
+            viewData.cars = data.map(value => value.dataValues);
+            // loop through viewData.cars and once we have found the vin that matches
+            // the personâ€™s "vin" value, add a "selected" property to the matching 
+            // viewData.cars object
+            for (let i = 0; i < viewData.cars.length; i++) {
+                if (viewData.cars[i].vin == viewData.person.vin) {
+                    viewData.cars[i].selected = true;
+                }
+            }
+    }).then(() => {
+        if (viewData.person == null) { // if no person - return an error
+            res.status(404).send("Person Not Found");
+        } else {
+            res.render("person", { viewData: viewData }); // render the "person" view
+        }
+    }).catch(() => {
+        viewData.person = null;
+        viewData.cars = []; // set cars to empty if there was an error
+        res.status(404).send("Person Not Found");
+    });
+});
+
+app.post("/person/update", (req, res) => {
+    data.updatePerson(req.body).then(()=>{
+    res.redirect("/people");
+  }).catch((err)=>{
+    res.status(500).send("Unable to Update the Person");
+  });;
+  
+});
+
+app.get("/people/delete/:id", (req,res)=>{
+    data.deletePeopleById(req.params.id).then(()=>{
+      res.redirect("/people");
+    }).catch((err)=>{
+      res.status(500).send("Unable to Remove Person / Person Not Found");
+    });
+  });
+
+
+
+
+/* Cars Routes */
+app.get("/cars", (req,res) => {
+    if (req.query.vin){
+        data.getCarsByVin(req.query.vin).then((data)=>{
+            res.render("cars", (data.length > 0) ? {cars:data.map(value => value.dataValues)} : { message: "no results" });
+        }).catch((err)=>{res.render("cars",{ message: "There was an error" })});
+    }
+    else if (req.query.year){
+        data.getCarsByYear(req.query.year).then((data)=>{
+            res.render("cars", (data.length > 0) ? {cars:data.map(value => value.dataValues)} : { message: "no results" });
+        }).catch((err)=>{res.render("cars",{ message: "There was an error" })});
+    }
+    else if (req.query.make){
+        data.getCarsByMake(req.query.make).then((data)=>{
+            res.render("cars", (data.length > 0) ? {cars:data.map(value => value.dataValues)} : { message: "no results" });
+        }).catch((err)=>{res.render("cars",{ message: "There was an error" })});
+    }
+    else{
+        data.getCars().then((data)=>{
+            res.render("cars", (data.length > 0) ? {cars:data.map(value => value.dataValues)} : { message: "no results" });
+        }).catch((err)=>{res.render("cars",{ message: "There was an error" })});
+    }
+});
+app.get("/cars/add", (req,res) => {
+        res.render("addCar");
+});
+
+app.post("/car/add", (req, res) => {
+    data.addCar(req.body).then(()=>{
+      res.redirect("/cars");
+    }).catch((err)=>{
+        res.status(500).send("Unable to Add the Car");
+      });
+});
+
+
+
+app.post("/car/update", (req, res) => {
+    data.updateCar(req.body).then(()=>{
+    res.redirect("/cars");
+  }).catch((err)=>{
+    res.status(500).send("Unable to Update the Car");
+  });
+  
+});
+
+app.get("/cars/delete/:vin", (req,res)=>{
+    data.deleteCarByVin(req.params.vin).then(()=>{
+      res.redirect("/cars");
+    }).catch((err)=>{
+      res.status(500).send("Unable to Remove Car / Car Not Found");
+    });
+  });
+
+
+
+/*Stores Route*/
+app.get("/stores", (req,res) => {
+    if (req.query.retailer){
+        data.getStoresByRetailer(req.query.retailer).then((data)=>{
+            res.render("stores", (data.length > 0) ? {stores:data.map(value => value.dataValues)} : { message: "no results" });
+        }).catch((err)=>{res.send(err)});
+    }
+    else{
+        data.getStores().then((data)=>{
+            res.render("stores", (data.length > 0) ? {stores:data.map(value => value.dataValues)} : { message: "no results" });
+        }).catch((err)=>{res.send(err)});
+    }
+});
+
+app.get("/stores/add", (req,res) => {
+    res.render("addStore");
+});
+
+app.post("/stores/add", (req, res) => {
+    data.addStore(req.body).then(()=>{
+        res.redirect("/stores");
+    }).catch((err)=>{
+        res.status(500).send("Unable to Add the Store");
+      });
+});
+
+app.get("/store/:id", (req,res)=>{
+    data.getStoreById(req.params.id).then((data)=>{
+        res.render("store", {store:data.map(value => value.dataValues)[0]});
+    }).catch(()=>{
+        res.status(404).send("Store Not Found");
+    });
+});
+
+app.post("/store/update", (req, res) => {
+    data.updateStore(req.body).then(()=>{
+        res.redirect("/stores");
+    }).catch((err)=>{
+        res.status(500).send("Unable to Update the Store");
+      });
+
+});
+
+app.get("/stores/delete/:id", (req,res)=>{
+    data.deleteStoreById(req.params.id).then(()=>{
+        res.redirect("/stores");
+    }).catch((err)=>{
+        res.status(500).send("Unable to Remove Store / Store Not Found");
+    });
+});
+
+
+
+
+
+/***404 routes and initialize**/
+app.use((req, res) => {
+    res.status(404).send("Page Not Found");
+  });
+
+data.initalize().then((data)=>{app.listen(HTTP_PORT, onHttpStart);})
+.catch((data)=>{console.log(data);})
+
